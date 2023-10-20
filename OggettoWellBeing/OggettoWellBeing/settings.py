9937530a7ba270e25edd83cea5c23a5fac061181
kirill <userkirill609@gmail.com>
2023-10-21 00:22:29 +0300

    add body of oauth with Google

--- OggettoWellBeing/OggettoWellBeing/settings.py
+++ OggettoWellBeing/OggettoWellBeing/settings.py
@@ -11,10 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
-
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auth.apps.AuthConfig'
+    'api_authentication.apps.ApiAuthenticationConfig',
+    'rest_framework'
+
 ]
 
 MIDDLEWARE = [
@@ -103,9 +101,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
