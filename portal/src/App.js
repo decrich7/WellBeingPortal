4bd165bb8730cf21261985c9c4c2e3f7c984e1ca
ZuZaaa228 <lukjanov-danilrambler.ru@yandex.ru>
2023-10-22 00:47:14 +0300

    event

--- portal/src/App.js
+++ portal/src/App.js
@@ -18,6 +18,7 @@ let cardsList = [
         photo: "https://stihi.ru/pics/2020/07/02/6618.jpg",
         topic: "Бойцовский клуб",
         statusPlace: true,
+        typeEvent: "video"
     },
     {
         id: 2,
@@ -28,6 +29,7 @@ let cardsList = [
         photo: "https://stihi.ru/pics/2020/07/02/6618.jpg",
         topic: "Каки пуки",
         statusPlace: "isLive",
+        typeEvent: "stream"
     },
     {
         id: 3,
@@ -38,6 +40,7 @@ let cardsList = [
         photo: "https://stihi.ru/pics/2020/07/02/6618.jpg",
         topic: "Бойцовский клуб",
         statusPlace: false,
+        typeEvent: "video"
     },
     {
         id: 4,
@@ -48,6 +51,7 @@ let cardsList = [
         photo: "https://stihi.ru/pics/2020/07/02/6618.jpg",
         topic: "Психи",
         statusPlace: true,
+        typeEvent: "link"
     },
 ]
 
@@ -90,21 +94,21 @@ function App() {
                 </Route>
                 <Route path="/cardInformations">
                     <Route path=":id" element={(
-                    <div className="App flex justify-center space-y-4">
-                        <div className="flex items-center flex-col justify-center w-5/6">
-                            <Header></Header>
-                            <CardInfo listEvent={cardsList}></CardInfo>
+                        <div className="App flex justify-center space-y-4">
+                            <div className="flex items-center flex-col justify-center w-5/6">
+                                <Header></Header>
+                                <CardInfo listEvent={cardsList}></CardInfo>
+                            </div>
                         </div>
-                    </div>
-                        )}>
-                </Route>
+                    )}>
+                    </Route>
 
 
                 </Route>
 
-                </Routes>
-                </BrowserRouter>
-                );
-            }
+            </Routes>
+        </BrowserRouter>
+    );
+}
 
 export default App;
--- portal/src/components/cards/CardInfo.jsx
+++ portal/src/components/cards/CardInfo.jsx
@@ -6,24 +6,41 @@ const CardInfo = ({...props}) => {
     const id = parseInt(params.id)
     const curEvent = props.listEvent.filter((el) => el.id === id)[0]
 
-    return (
-        <div className="CardInformations items-start w-full">
-            <div>
-                {curEvent.name}
-            </div>
-            <div className="Video">
-                <video className="w-full h-auto sm:min-w-[70%] 2xl:max-w-[50%] xl:max-w-[50%] border border-gray-200 rounded-lg dark:border-gray-700" controls>
+    return (<div className="CardInformations items-start w-full">
+        <div>
+            {curEvent.name}
+        </div>
+        <div className="video-chat flex-wrap place-content-around">
+            <div className="Video sm:min-w-[60%] 2xl:max-w-[50%] xl:max-w-[50%]">
+                <video
+                    className="w-full h-auto  border border-gray-200 rounded-lg dark:border-gray-700"
+                    controls>
                     <source src="../../../media/пена.mp4"
                             type="video/mp4"
                     />
                 </video>
             </div>
+            {(curEvent.typeEvent == "stream") ? (
+                <div className="Chat w-[30%] place-self-end">
+                    Тут чат
+                </div>
+                ) : (
+                    <></>
+                )}
             <div>
                 {curEvent.info}
             </div>
+            <div className="questions">
+                <div className="questions-text">
+                    Задать вопрос
+                </div>
+                <div className="questions-form">
 
+                </div>
+            </div>
         </div>
-    )
+
+    </div>)
 }
 
 export default CardInfo;
