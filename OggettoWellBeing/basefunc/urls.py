629810dae4e8ac52bd78b8d69cf3c13093d67cdc
kirill <userkirill609@gmail.com>
2023-10-21 13:03:37 +0300

    add crud for models

---
+++ OggettoWellBeing/basefunc/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import EventViewSet, TopicViewSet, QuestViewSet, ClubViewSet, ExpertViewSet
+
+router = routers.SimpleRouter()
+router.register(r'event', EventViewSet)
+router.register(r'topic', TopicViewSet)
+router.register(r'quest', QuestViewSet)
+router.register(r'club', ClubViewSet)
+router.register(r'expert', ExpertViewSet)
+
+urlpatterns = [
+    path('api/v1/', include(router.urls)),
+]
