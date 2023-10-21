d1af40f9acd4a44c09d558565a5b0ab833543067
EugeneSvetov <evgenii.tbook@gmail.com>
2023-10-21 15:34:15 +0300

    merged

--- portal/src/components/cards/Cards.jsx
+++ portal/src/components/cards/Cards.jsx
@@ -34,6 +34,22 @@ let cardsList = [
         expert: "Пидорас И.И.",
         photo: "https://stihi.ru/pics/2020/07/02/6618.jpg"
     },
+    {
+        id: 5,
+        name: "швепс",
+        info: "Бесплатая раздача курс хурс хруст пуки каки жопи писи каки пипи",
+        shedule: "15 апреля 18:00",
+        expert: "Пидорас И.И.",
+        photo: "https://stihi.ru/pics/2020/07/02/6618.jpg"
+    },
+    {
+        id: 6,
+        name: "швепс",
+        info: "Бесплатая раздача курс хурс хруст пуки каки жопи писи каки пипи",
+        shedule: "15 апреля 18:00",
+        expert: "Пидорас И.И.",
+        photo: "https://stihi.ru/pics/2020/07/02/6618.jpg"
+    },
 ]
 
 const Cards = (...props) => {
